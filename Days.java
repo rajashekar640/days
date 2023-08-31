31f89ede24e81382da78238b3c5bb183205ddf5f
Rajashekar Bathine <rajashekarb0509@gmail.com>
2023-08-31 15:30:05 -0400

    done

---
+++ Days.java
@@ -0,0 +1,38 @@
+class Days
+{
+
+
+     public static void main (String arg[])
+     {
+    
+
+
+        int a=1;
+      
+        switch(a) {
+            case 1:
+                       System.out.println("january");
+                            break;
+            case 2:
+                     System.out.println("feburary");
+                         break;
+            case 3:
+                     System.out.println("Wednesday");
+                       break;
+            case 4:
+                         System.out.println("Thursday");
+                        break;
+            case 5:
+                       System.out.println("Friday");
+                       break;
+            case 6:
+                       System.out.println("Saturday");
+                        break;
+            case 9:
+                        System.out.println("Sunday");
+        }
+    }
+}
+
+           
+
